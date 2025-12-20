a2d4e857e2a1c5d1aba4e6b512022040ec68aa4a
Osman Ghani Granday <osmamghani009009@gmail.com>
2025-12-20 10:23:09 +0500

    m

--- newFile.py
+++ newFile.py
@@ -3,16 +3,14 @@ def sum(num, num1):
 
 
 def sub(num, num1):
-    return num + num1
+    return num - num1
 
 
 a = input("enter number 1 for SUM and 2 for Subtraction: ")
+num = int(input("enter first number to add: "))
+num1 = int(input("enter second number to add: "))
 
 if a == "1":
     print("result: ", sum(num, num1))
 if a == "2":
     print("result: ", sub(num, num1))
-
-
-num = int(input("enter first number to add: "))
-num1 = int(input("enter second number to add: "))
