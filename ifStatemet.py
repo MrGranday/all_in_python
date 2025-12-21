3ceb12b21a600421b16fe870a53e928a6e84fa85
Osman Ghani Granday <osmamghani009009@gmail.com>
2025-12-21 19:34:03 +0500

    the if statement or the else statement

--- ifStatemet.py
+++ ifStatemet.py
@@ -12,3 +12,17 @@ if x > 3:
     print("x is bigger then 3 ")
 if x <= 3:
     print("x is equal or smaller then 3 ")
+
+
+# this is else if, for brief elif
+
+def compare_to_five(y):
+    if y > 5:
+        return "y is greater"
+    elif y < 5:
+        return "y is less "
+    else:
+        return "this was equal"
+
+
+print(compare_to_five(10))
