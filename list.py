f5107c413e2c141ccf8524304ee875683eb12d36
Osman Ghani Granday <osmamghani009009@gmail.com>
2025-12-22 21:41:55 +0500

    lists with strings and numbers

--- list.py
+++ list.py
@@ -1,2 +1,21 @@
 the_list = ['osman', 'ghani', 'granday']
 print(the_list)
+print(the_list[1])
+
+
+# replacing the index
+# so this will replace the late name GRANDAY with OMAR
+the_list[2] = 'OMAR'
+print(the_list)
+
+
+# if we want to delete the index
+del the_list[1]
+print(the_list)
+
+# so we have deleted the second index or element
+
+
+numbers = [1, 2, 3, 4, 5]
+print(numbers)
+print(numbers[3])
