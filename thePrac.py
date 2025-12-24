de6ca88223fe1c8800a83e9a37f485282b878881
Osman Ghani Granday <osmamghani009009@gmail.com>
2025-12-24 09:48:24 +0500

    the prac of fun, list, Dic, Iter

--- thePrac.py
+++ thePrac.py
@@ -1,15 +1,35 @@
-y = 5**3
-print(y)
-theAs = y == 125
-print(theAs)
-# this is comment not the code
-print(1, 3)
+# Step 1: Create a function
+# Step 2: Function should take one number as input
+# Step 3: Use if condition
+# Step 4: If number % 2 == 0 → print "Even"
+# Step 5: Else → print "Odd"
 
 
-# Comparison Operators
+def number(num):
+    if num % 2 == 0:
+        return 'Even'
+    else:
+        return 'Odd'
 
-c = 10 == 20/2
-print(c)
 
-c = 10 != 10
-print(c)
+print(number(10))
+
+
+# Step 1: Create a list of numbers
+# Step 2: Create a variable sum = 0
+# Step 3: Use for loop to go through list
+# Step 4: Add each number to sum
+# Step 5: Print final sum
+
+
+list_num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 23, 45, 12, 78, 4, 23, 65]
+
+
+def sum_of(list_num):
+    sum = 0
+    for n in list_num:
+        sum = sum + n
+    return sum
+
+
+print(sum_of(list_num))
